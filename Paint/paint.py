7983ee7e12ff8d32e92b7f5f1ba4d3ad0bafe0f0
A01800182 <A01800182@tec.mx>
2024-03-21 11:09:29 -0600

    Se Comento: Paint

--- Paint/paint.py
+++ Paint/paint.py
@@ -9,11 +9,14 @@ Exercises
 5. Add width parameter.
 """
 
+# Se import turtle
 from turtle import *
 
+# Se importa vector de freegames
 from freegames import vector
 
 
+# Esta funcion permite dibujar la linea trazada por el usuario
 def line(start, end):
     """Draw line from start to end."""
     up()
@@ -21,7 +24,7 @@ def line(start, end):
     down()
     goto(end.x, end.y)
 
-
+# Esta función permite que, dado los dos extremos inferiores de un cubo, el programa termina de dibujar la figura y la rellena 
 def square(start, end):
     """Draw square from start to end."""
     up()
@@ -50,7 +53,7 @@ def triangle(start, end):
     """Draw triangle from start to end."""
     pass  # TODO
 
-
+# Con esta funcion, guardamos la posición de inicio de la figura y establecemos que el tipo de figura es la de 'linea'
 def tap(x, y):
     """Store starting point or draw shape."""
     start = state['start']
@@ -64,21 +67,26 @@ def tap(x, y):
         state['start'] = None
 
 
+# Esta función nos  permite cambiar los valores del diccionario 'state', el cual guarda los valores de posicion inicial y tipo de figura
 def store(key, value):
     """Store value in state at key."""
     state[key] = value
 
-
+# Inicializamos el diccionaro 'state', el cual guarda los valores de posicion inicial y tipo de figura
 state = {'start': None, 'shape': line}
 setup(420, 420, 370, 0)
 onscreenclick(tap)
 listen()
 onkey(undo, 'u')
+
+# Estas funciones lambda nos permiten cambiar el color de la figura
 onkey(lambda: color('black'), 'K')
 onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+
+# Estas funciones lambda nos permiten cambiar el tipo de figura
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
