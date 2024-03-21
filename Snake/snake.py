6c811ac11a24cd48c9004fc097ee275466c5178c
PeritiaCodex <A01800476@tec.mx>
2024-03-21 10:44:58 -0600

    Se Comento: Snake

--- Snake/snake.py
+++ Snake/snake.py
@@ -8,32 +8,35 @@ Exercises
 4. Change the snake to respond to mouse clicks.
 """
 
+# Aqui hace los imports necesarios de las librerias par que funcione el codigo
 from random import randrange
 from turtle import *
 
 from freegames import square, vector
 
+# Declara las variables de antemano
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
-
+# Define la funcion de cambio de direccion  de la serpiente
 def change(x, y):
     """Change snake direction."""
     aim.x = x
     aim.y = y
 
-
+# Define la funcion "inside" que retorna un verdadero si la cabeza esta dentro del rango
 def inside(head):
     """Return True if head inside boundaries."""
     return -200 < head.x < 190 and -200 < head.y < 190
 
-
+# Define la funcion "move" que indica  la direccion a la que se movera
 def move():
     """Move snake forward one segment."""
     head = snake[-1].copy()
     head.move(aim)
 
+    # Aqui define que pasa si choca consigo misma, se pone en rojo
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, 'red')
         update()
@@ -41,6 +44,7 @@ def move():
 
     snake.append(head)
 
+    # Define el if si come el puntito, crece un cuadrado
     if head == food:
         print('Snake:', len(snake))
         food.x = randrange(-15, 15) * 10
@@ -50,6 +54,7 @@ def move():
 
     clear()
 
+    # Define el color del cuerpo de la serpiente y el de la comida
     for body in snake:
         square(body.x, body.y, 9, 'black')
 
@@ -57,7 +62,7 @@ def move():
     update()
     ontimer(move, 100)
 
-
+# Define los valores iniciales y los de cambio de moviento con las flechas
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
